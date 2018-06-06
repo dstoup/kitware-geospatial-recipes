a21a44ce436619bd1592075d7db6626cb53c5d74
Dan Lipsa <dan.lipsa@kitware.com>
2018-06-06 13:50:17 -0400

    Depend on our vtk package.

--- recipes/texture-atlas/build.sh
+++ recipes/texture-atlas/build.sh
@@ -5,6 +5,16 @@ cd build
 
 BUILD_CONFIG=Release
 
+if [ $(uname) == Darwin ]; then
+    COMP_CC=clang
+    COMP_CXX=clang++
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+fi
+
+
+
 cmake ../Texture_Atlas/src/ -G "Ninja" \
     -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
